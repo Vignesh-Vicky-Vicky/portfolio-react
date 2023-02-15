c8f0816270ea5e156cc8edfded3236d3a64aa40c
vignesh <karthikvignesh3@gmail.com>
2023-02-15 14:55:31 +0530

    added aboutus section

--- portfolio-react/src/pages/AboutUs.js
+++ portfolio-react/src/pages/AboutUs.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 // import component
-import AboutSection from '../components/AboutSection';
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
-    return (
-        <AboutSection />
-    )
-}
-
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
+};
 
 export default AboutUs;
